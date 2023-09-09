94168bc1d599487270073cd0d21f5cd9f06ff829
AnitaToRemember <best.anac@gmail.com>
2023-09-09 16:02:15 +0200

    <adding controller to show links by owner>

---
+++ src/controllers/links/listLinksByOwnerController.js
@@ -0,0 +1,24 @@
+// Importing models
+const selectAllLinkPostsModel = require('../../models/links/selectAllLinkPostsModel');
+
+// Final controller function that returns the list of entries created by the owner.
+const listLinksByOwnerController = async (req, res, next) => {
+    try {
+        // Get the user's ID from the authenticated user (assuming you have authentication middleware).
+        const userId = req.user.id;
+
+        // Fetch links created by the owner (user with userId)
+        const links = await selectAllLinkPostsModel('', userId);
+
+        res.send({
+            status: 'ok',
+            data: {
+                links,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = listLinksByOwnerController;
--- src/routes/linksRoutes.js
+++ src/routes/linksRoutes.js
@@ -20,6 +20,7 @@ const {
     votePostedLinkController, 
     deteleLinkByIdController
 } = require('../controllers/links');
+const listLinksByOwnerController = require('../controllers/links/listLinksByOwnerController');
 
 
 //Creating a new post with a link
@@ -27,6 +28,12 @@ router.post('/links', authUser, userExists, newLinkController);
 
 //Obtaining all the posts
 router.get('/links', authUserOptional, listLinksController);
+
+//Obtaining all the posts form one user
+router.get('/mylinks', authUser, listLinksByOwnerController);
+
+//Filter all the posts
+router.get('/links/filter', authUserOptional, listLinksController);
 
 //Selecting an specific link post by id
 router.get('/links/:linkId', authUserOptional, linkPostExists, getLinkController);
