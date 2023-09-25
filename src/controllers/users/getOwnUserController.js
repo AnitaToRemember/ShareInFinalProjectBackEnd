f4d41d1b72494cbe9baac54b63ecdee26b14f879
Evelin Arboleda <evelinarboleda23@gmail.com>
2023-09-25 12:23:49 +0200

    check if user has avatar if not no return avatar url

--- src/controllers/users/getOwnUserController.js
+++ src/controllers/users/getOwnUserController.js
@@ -5,7 +5,9 @@ const getOwnUserController = async (req, res, next) => {
     try {
         // Obtaining new user data.
         let user = await selectUserByIdModel(req.user.id);
-        user.avatar = `http://${req.headers.host}/${user.avatar}`;
+        if (user.avatar) {
+            user.avatar = `http://${req.headers.host}/${user.avatar}`;
+        }
 
         res.send({
             status: 'ok',
